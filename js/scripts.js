f98f65e089e64375a37afb732e9975a27c5d3ba4
Linus Mutugi <linusmutugi5178@gmail.com>
2020-02-21 23:34:35 +0300

    toogle complete,css and html modified

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".what .col-sm-3").click(function(){
+    $(".col-sm-3 h3").toggle();
+    $(".col-sm-3 p").toggle();
+  });
+  $(".what .col-sm-4").click(function(){
+    $(".col-sm-4 h3").toggle();
+    $(".col-sm-4 p").toggle();
+  });
+  $(".what .col-sm-5").click(function(){
+    $(".col-sm-5 h3").toggle();
+    $(".col-sm-5 p").toggle();
+  });
+});
